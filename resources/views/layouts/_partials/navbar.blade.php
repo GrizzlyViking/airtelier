b62f6685a531128f9f81e691d484d1b932f45ea7
Sebastian Scheel Edelmann <sebastian@edelmann.co.uk>
2019-08-06 18:47:54 +0200

    navbar update

--- resources/views/layouts/_partials/navbar.blade.php
+++ resources/views/layouts/_partials/navbar.blade.php
@@ -38,8 +38,16 @@
                             <a class="dropdown-item" href="{{ route('offers.create') }}">
                                 {{ __('Create an offer') }}
                             </a>
+                            <div class="dropdown-divider"></div>
                             <a class="dropdown-item" href="{{ route('articles.index') }}">
                                 {{ __('List of articles') }}
+                            </a>
+                            <a class="dropdown-item" href="{{ route('articles.create') }}">
+                                {{ __('Create an article') }}
+                            </a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="{{ route('articles.index') }}">
+                                {{ __('List of messa') }}
                             </a>
                             <a class="dropdown-item" href="{{ route('articles.create') }}">
                                 {{ __('Create an article') }}
